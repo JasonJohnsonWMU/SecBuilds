a83273324070eb03db094791d803251c8d2de0c6
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-02-14 16:22:54 -0500

    Update graylog.sh

--- GrayLog/graylog.sh
+++ GrayLog/graylog.sh
@@ -1,1 +1,31 @@
 
+# We'll install Greenbone in our home directory, so make sure we're in our home directory and update and upgrade
+cd ~
+sudo apt update
+sudo apt upgrade -y
+# We'll install Graylog v 5.0 and requires the following to maintain compatibility with its software dependencies: 
+# OpenJDK 17 (This is embedded in Graylog 5.0 and does not need to be separately installed.)
+# OpenSearch 1.x, 2.x or Elasticsearch 7.10.2 
+# MongoDB 5.x or 6.x
+
+#Installing MongDB 
+# 1- Import the public key used by the package management system.
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+# 2- Install gnupg and its required libraries using the following command:
+sudo apt-get install gnupg
+# 3- Once installed, retry importing the key:
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+
+#Create the /etc/apt/sources.list.d/mongodb-org-6.0.list file for Ubuntu 22.04 (Jammy):
+echo 'deb https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+#Reload local package database
+sudo apt-get update
+# Install MongoDB Packages 
+sudo apt-get install -y mongodb-org
+
+# run mongoDB
+sudo systemctl start mongod
+
+#check status 
+sudo systemctl status mongod
