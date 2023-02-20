fadd68eff36c8e73bc3af3b3c26467ab42ae2f2b
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-02-20 12:10:35 -0500

    Update graylog.sh

--- GrayLog/graylog.sh
+++ GrayLog/graylog.sh
@@ -34,3 +34,11 @@ sudo systemctl daemon-reload
 sudo systemctl enable mongod.service
 sudo systemctl restart mongod.service
 sudo systemctl --type=service --state=active | grep mongod
+
+#download Elasticsearch
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.2-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.2-linux-x86_64.tar.gz.sha512
+shasum -a 512 -c elasticsearch-7.10.2-linux-x86_64.tar.gz.sha512 
+tar -xzf elasticsearch-7.10.2-linux-x86_64.tar.gz
+cd elasticsearch-7.10.2
