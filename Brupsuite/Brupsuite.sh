e5e5d482e9716d49abe871553ca6ee9864b3b19c
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-03-21 17:42:25 -0400

    Update Brupsuite.sh

--- Brupsuite/Brupsuite.sh
+++ Brupsuite/Brupsuite.sh
@@ -6,3 +6,6 @@ sudo apt install python3-pip
 sudo pip install gdown
 sudo chmod +x  burpsuite_community_linux_v2023_2_3.sh
 sudo ./burpsuite_community_linux_v2023_2_3.sh 
+
+#Installing Java
+sudo apt install default-jre
