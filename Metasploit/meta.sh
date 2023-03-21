49b5194a58585c81f1c779699f53981f0ca616d1
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-03-21 16:44:52 -0400

    Create meta.sh

---
+++ Metasploit/meta.sh
@@ -0,0 +1,7 @@
+#Installing Metaspolit 
+
+wget http://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
+# set the execution permission
+chmod +x ./metasploit-latest-linux-x64-installer.run
+
+sudo ./metasploit-latest-linux-x64-installer.run
