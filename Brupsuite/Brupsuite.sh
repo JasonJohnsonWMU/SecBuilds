986a96e8e80b0146669e2a395796c3c0e88b87ba
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-03-21 17:34:47 -0400

    Update Brupsuite.sh

--- Brupsuite/Brupsuite.sh
+++ Brupsuite/Brupsuite.sh
@@ -1,1 +1,8 @@
 
+#Installing PIP 3 
+
+sudo apt install python3-pip
+#Install gdown
+sudo pip install gdown
+sudo chmod +x  burpsuite_community_linux_v2023_2_3.sh
+sudo ./burpsuite_community_linux_v2023_2_3.sh 
