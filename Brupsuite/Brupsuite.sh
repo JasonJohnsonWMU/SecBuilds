e114512142590a8b5f49d04185e01ef52872a9dd
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-03-30 01:47:53 -0400

    Update Brupsuite.sh

--- Brupsuite/Brupsuite.sh
+++ Brupsuite/Brupsuite.sh
@@ -4,8 +4,12 @@
 sudo apt install python3-pip
 #Install gdown
 sudo pip install gdown
+gdown https://drive.google.com/uc?id=1kn1xQVVgnU_354kpRSHKRyDw_O6a-6NM
 sudo chmod +x  burpsuite_community_linux_v2023_2_3.sh
 sudo ./burpsuite_community_linux_v2023_2_3.sh 
 
-#Installing Java
+#Installing Java to run burp
 sudo apt install default-jre
+#Launch Burp from terminal, Preferd to run from the main screen
+#cd ../../usr/local/BurpSuiteCommunity
+#sudo java -jar -Xmx4g burpsuite_community.jar
