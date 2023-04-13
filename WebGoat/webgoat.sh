4863b8556ca9584961e0bcf4e72c756676f78c01
ahmadkhanfarCs <124195683+ahmadkhanfarCs@users.noreply.github.com>
2023-04-12 23:03:58 -0400

    Update webgoat.sh

--- WebGoat/webgoat.sh
+++ WebGoat/webgoat.sh
@@ -70,3 +70,9 @@ sudo tar xzvf openjdk-11_linux-x64_bin.tar.gz --directory /usr/lib/jvm
 sudo ln -s /usr/lib/jvm/jdk-11/bin/java /usr/bin/java
 
 
+#Cleaning up
+sudo rm openjdk-11_linux-x64_bin.tar.gz 
+#Intall Webgoat V2023.4
+sudo wget https://github.com/WebGoat/WebGoat/releases/download/v2023.4/webgoat-2023.4.jar
+#Update JDK 
+sudo apt install openjdk-18-jre-headless
